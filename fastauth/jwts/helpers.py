a77d631422fbdf9ad4c3bbf4b69e12d78c59ca5b
ashgw <ashrefgw@proton.me>
2024-01-15 17:41:14 +0100

    secret encryption key's implementation set

--- fastauth/jwts/helpers.py
+++ fastauth/jwts/helpers.py
@@ -5,7 +5,7 @@ from fastauth.exceptions import WrongKeyLength
 
 def generate_secret() -> str:
     """
-    use this function o generate a valid secret key for the auth flow
+    use this function to generate a valid secret key for the auth flow
     """
     characters = ascii_letters + "_-?$#+-*"
     return "".join(choice(characters) for _ in range(32))
@@ -14,7 +14,3 @@ def generate_secret() -> str:
 def check_key_length(key: str) -> None:
     if len(key) != 32:
         raise WrongKeyLength("Key length must be exactly 32 bit")
-
-
-if __name__ == '__main__':
-    print(generate_secret())
