3a124bc16ea8a6a22eef199c5ea934b75a85beab
ashgw <ashrefgw@proton.me>
2024-01-17 05:55:55 +0100

    add QueryParams type

--- fastauth/utils.py
+++ fastauth/utils.py
@@ -2,8 +2,8 @@ from fastauth.providers.base import Provider
 from hashlib import sha256
 from secrets import token_urlsafe
 from base64 import urlsafe_b64encode
-from typing import Dict, Optional
-from fastauth._types import OAuthParams
+from typing import Optional, Dict
+from fastauth._types import OAuthParams, QueryParams
 
 def auth_cookie_name(*, cookie_name: str) -> str:
     return "fastauth" + "." + cookie_name
@@ -22,27 +22,27 @@ def gen_csrf_token() -> str:
     return token_urlsafe(64)[:84]
 
 
-def querify_kwargs(kwargs: Optional[Dict[str, str]] = None) -> str:
+def querify_kwargs(kwargs: Optional[QueryParams] = None) -> str:
     if kwargs is None:
         return ""
     query_string = "&".join([f"{key}={value}" for key, value in sorted(kwargs.items())])
     return f"&{query_string}"
 
 
-# TODO: to be refactored
 def tokenUrl_payload(
     *,
     provider: Provider,
     **kwargs: str,
-) -> Dict[str, str]:
-    extra_args = {key: value for _, (key, value) in enumerate(kwargs.items(), start=1)}
-    return {
-        "grant_type": provider.grant_type,
-        "client_id": provider.client_id,
-        "client_secret": provider.client_secret,
-        "redirect_uri": provider.redirect_uri,
+) -> QueryParams:
+    extra_args: Dict[str,str] = {key: value for _, (key, value) in enumerate(kwargs.items(), start=1)}
+    qp =  QueryParams(
+        grant_type= provider.grant_type,
+        client_id= provider.client_id,
+        client_secret= provider.client_secret,
+        redirect_uri= provider.redirect_uri,
         **extra_args,
-    }
+    )
+    return qp
 
 
 def base_redirect_url(
@@ -54,7 +54,7 @@ def base_redirect_url(
     state: str,
     code_challenge: str,
     code_challenge_method: str,
-    kwargs: Dict[str, str],
+    kwargs: QueryParams,
 ) -> str:
     return (
         f"{authorizationUrl}?"
