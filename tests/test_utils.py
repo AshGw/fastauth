b8d59655efdb9751440ae02f300a700fec6307b4
ashgw <ashrefgw@proton.me>
2024-01-17 21:06:51 +0100

    querify kwargs tests set

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from fastauth import utils
+from fastauth.types import QueryParams
 
 def test_auth_cookie_name():
     c_name = uuid4().hex
@@ -23,7 +24,16 @@ def test_csrf():
 
 
 def test_querify_kwargst():
-    res = utils.querify_kwargs({'x':'one','y':'two'})
-    assert res == '&x=one&y=two'
+    res = utils.querify_kwargs(QueryParams(
+        grant_type= "grant_type",
+        client_id= "client_id",
+        client_secret= "client_secret",
+        redirect_uri= "redirect_uri",
+        extraOne="one"))
+    assert res == ('&client_id=client_id'
+                   '&client_secret=client_secret'
+                   '&extraOne=one'
+                   '&grant_type=grant_type'
+                   '&redirect_uri=redirect_uri')
     empty_res = utils.querify_kwargs()
     assert empty_res == ''
