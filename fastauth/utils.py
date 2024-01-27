66760597792599aecd680ec690947efd46d260e5
AshGw <AshrefGw@proton.me>
2024-01-27 03:22:51 +0100

    use the native provider names to determine which one to log

--- fastauth/utils.py
+++ fastauth/utils.py
@@ -4,27 +4,29 @@ from base64 import urlsafe_b64encode
 from fastauth._types import OAuthParams, QueryParams
 from typing import TypeVar, ParamSpec, Callable, Optional
 from fastauth.providers.base import Provider
+from functools import wraps
 
 _T = TypeVar("_T")
 _PSpec = ParamSpec("_PSpec")
 
 
 def log_action(f: Callable[_PSpec, _T]) -> Callable[_PSpec, _T]:
+    @wraps(f)
     def wrap(*args: _PSpec.args, **kwargs: _PSpec.kwargs) -> _T:
         provider = next((arg for arg in args if isinstance(arg, Provider)), None)
         if not provider:
             return f(*args, **kwargs)
-        if f.__name__ == "authorize":
+        if f.__name__ == provider.authorize.__name__:
             provider.logger.info(
                 f"Redirecting the client to the resource owner via"
                 f" {provider.provider} authorization server"
             )
-        if f.__name__ == "get_access_token":
+        if f.__name__ == provider.get_access_token.__name__:
             provider.logger.info(
                 f"Requesting the access token from {provider.provider} "
                 f"authorization server"
             )
-        if f.__name__ == "get_user_info":
+        if f.__name__ == provider.get_user_info.__name__:
             provider.logger.info(
                 f"Requesting user information from {provider.provider} "
                 f"resource server"
