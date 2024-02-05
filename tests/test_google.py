0f52d1b874c6b2fb39d284d7623dd8cc8f360776
AshGw <AshrefGw@proton.me>
2024-02-05 04:41:17 +0100

    cleanup g tests

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,3 +1,7 @@
+from unittest.mock import AsyncMock
+from unittest.mock import patch
+
+
 import pytest
 from typing import cast, Dict, Any
 
@@ -14,6 +18,7 @@ from pydantic import ValidationError
 from fastauth.exceptions import (
     InvalidUserInfoAccessRequest,
     InvalidTokenAcquisitionRequest,
+    SchemaValidationError,
 )
 from fastauth.data import StatusCode
 from fastauth.utils import gen_oauth_params
@@ -55,36 +60,57 @@ def valid_user_data() -> Dict[str, Any]:
     ).dict()
 
 
-def test_serialize(valid_user_data) -> None:
-    valid_data = valid_user_data
-    expected_result = {
-        "user_id": "123",
-        "email": "example@gmail.com",
+@pytest.fixture
+def invalid_user_data() -> Dict[str, str]:
+    return {
+        "id": "123",
+        "email": "not@gmail",  #
+        "verified_email": True,
         "name": "John Doe",
-        "avatar": "https://example.com/hosted/pic",
-        "extras": {
-            "locale": "en",
-            "verified_email": True,
-            "given_name": "John",
-            "family_name": "Doe",
-        },
+        "given_name": "John",
+        "family_name": "Doe",
+        "picture": "htps://lh3.googleusercontent.com/a/abc",  #
+        "locale": "en",
     }
-    assert serialize_user_info(valid_data) == expected_result
-    # now if data is invalid e.g avatar is not presented as a URL then
 
-    with pytest.raises(ValidationError):
-        serialize_user_info(
-            GoogleUserJSONData(
-                email="example@gmail",  # type: ignore   # invalid email
-                verified_email=True,
-                given_name="John",
-                family_name="Doe",
-                picture="htps://example.com/hosted/pic",  # type: ignore   # not an actual HTTP(s) URL
-                locale="en",
-                id="123",
-                name="John Doe",
-            ).dict()
-        )
+
+@pytest.mark.asyncio
+async def test_user_info_acquisition(valid_user_data, google) -> None:
+    with patch(
+        "fastauth.providers.google.google.Google._request_user_info"
+    ) as mock_inf_request:
+        mock_response = AsyncMock()
+        mock_response.status_code = 200
+        mock_response.json = valid_user_data
+        mock_inf_request.return_value = mock_response
+        _request_info = await google._request_user_info(access_token="...")
+        json = _request_info.json
+        status = _request_info.status_code
+        assert json == valid_user_data
+        assert status == 200
+        user_info = await google.get_user_info(access_token="...")
+        assert user_info == serialize_user_info(json)
+
+
+@pytest.mark.asyncio
+async def test_user_info_acquisition(invalid_user_data, google) -> None:
+    with patch(
+        "fastauth.providers.google.google.Google._request_user_info"
+    ) as mock_inf_request:
+        mock_response = AsyncMock()
+        mock_response.status_code = 200
+        mock_response.json = invalid_user_data
+        mock_inf_request.return_value = mock_response
+        _request_info = await google._request_user_info(access_token="...")
+        json = _request_info.json
+        status = _request_info.status_code
+        assert json == invalid_user_data
+        assert status == 200
+        Config.debug = True  # raise in debug
+        with pytest.raises(SchemaValidationError):
+            _ = await google.get_user_info(access_token="valid_one")
+        Config.debug = False  # hush in  normal
+        assert await google.get_user_info(access_token="valid_one") is None
 
 
 @pytest.mark.asyncio
@@ -125,3 +151,35 @@ async def test_user_info_with_invalid_token(google):
     Config.debug = False
     _ = await google.get_user_info(access_token="...")
     assert _ is None
+
+
+def test_serialize(valid_user_data) -> None:
+    valid_data = valid_user_data
+    expected_result = {
+        "user_id": "123",
+        "email": "example@gmail.com",
+        "name": "John Doe",
+        "avatar": "https://example.com/hosted/pic",
+        "extras": {
+            "locale": "en",
+            "verified_email": True,
+            "given_name": "John",
+            "family_name": "Doe",
+        },
+    }
+    assert serialize_user_info(valid_data) == expected_result
+    # now if data is invalid e.g avatar is not presented as a URL then
+
+    with pytest.raises(ValidationError):
+        serialize_user_info(
+            GoogleUserJSONData(
+                email="example@gmail",  # type: ignore   # invalid email
+                verified_email=True,
+                given_name="John",
+                family_name="Doe",
+                picture="htps://example.com/hosted/pic",  # type: ignore   # not an actual HTTP(s) URL
+                locale="en",
+                id="123",
+                name="John Doe",
+            ).dict()
+        )
