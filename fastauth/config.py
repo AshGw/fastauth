9d2a2244660b3c83c5f41384c343f4e07560047a
AshGw <AshrefGw@proton.me>
2024-02-18 23:47:09 +0100

    set `_DefaultAttrs` for `FastAuthConfig`

--- fastauth/config.py
+++ fastauth/config.py
@@ -9,8 +9,8 @@ class FastAuthConfig:
     debug: ClassVar[bool] = True
 
     @classmethod
-    def get_defaults(cls) -> _DefaultParams:
-        return _DefaultParams(debug=cls.debug, logger=cls.logger)
+    def get_defaults(cls) -> _DefaultAttrs:
+        return _DefaultAttrs(debug=cls.debug, logger=cls.logger)
 
     @classmethod
     def set_defaults(cls, debug: bool, logger: Logger) -> None:
@@ -18,6 +18,6 @@ class FastAuthConfig:
         cls.logger = logger
 
 
-class _DefaultParams(NamedTuple):
+class _DefaultAttrs(NamedTuple):
     debug: bool
     logger: Logger
