97bb4c093b6ec28cdf6074dffd0ce974c5772bc9
AshGw <AshrefGw@proton.me>
2024-02-22 18:47:44 +0100

    omit plain   from

--- fastauth/adapters/use_response.py
+++ fastauth/adapters/use_response.py
@@ -6,7 +6,7 @@ from fastauth.adapters.fastapi.use_response import use_fastapi_response
 
 def use_response(
     framework: Framework,
-    response_type: Literal["normal", "json", "redirect"] = "normal",
+    response_type: Literal["json", "redirect"],
 ) -> Type[FastAPIBaseResponse]:
     if isinstance(framework, FastAPI):
         return use_fastapi_response(response_type)
