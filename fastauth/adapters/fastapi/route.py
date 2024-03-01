99be34654f6f8fd02add4c1d084d37fda0e62178
AshGw <AshrefGw@proton.me>
2024-03-02 00:47:22 +0100

    fix return types of custom FastAPI route handler

--- fastauth/adapters/fastapi/route.py
+++ fastauth/adapters/fastapi/route.py
@@ -1,11 +1,11 @@
-from typing import Callable, Coroutine, Any
+from typing import Callable, Coroutine
 from fastapi.routing import APIRoute
 from fastauth.adapters.fastapi.request import FastAPIRequest
 from fastapi import Request, Response
 
 
 class FastAuthRoute(APIRoute):
-    def get_route_handler(self) -> Callable[[Request], Coroutine[Any, Any, Response]]:
+    def get_route_handler(self) -> Callable[[Request], Coroutine[None, None, Response]]:
         original_route_handler = super().get_route_handler()
 
         async def fastauth_route_handler(request: Request) -> Response:
