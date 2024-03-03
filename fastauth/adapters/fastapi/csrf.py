5b020a7544445c99f68249e431c0566d63d1fcc1
AshGw <AshrefGw@proton.me>
2024-03-03 18:31:05 +0100

    use a singleton for the CSRF Token data

--- fastauth/adapters/fastapi/csrf.py
+++ fastauth/adapters/fastapi/csrf.py
@@ -6,7 +6,6 @@ from os import urandom
 from typing import Awaitable, Callable
 from starlette.responses import Response
 from fastauth.const_data import CookieData, StatusCode
-from fastauth._types import FallbackSecrets
 from fastauth.utils import name_cookie
 from fastauth.config import FastAuthConfig
 
@@ -25,25 +24,18 @@ JWT_EMBEDDED = urandom(16).hex()
 
 @final
 class CSRFValidationFilter(CSRF, FastAuthConfig):
-    def __init__(
-        self, request: Request, response: Response, secrets: FallbackSecrets
-    ) -> None:
+    def __init__(self, request: Request, response: Response) -> None:
         self.request = request
         self.response = response
-        self.secrets = secrets
-        self.jwt_embedded_value = JWT_EMBEDDED
-        self.collision_value = urandom(16).hex()
 
     def __call__(self) -> None:
         token = self.get_csrf_token()
         if token is None:
             self.set_csrf_token_cookie()
-            self.reject(reason=REASON_NO_CSRF_COOKIE, request=self.request)
-            return
-        if not self.validate_csrf_token(token, secrets=self.secrets):
-            self.reject(reason=REASON_BAD_TOKEN, request=self.request)
-            return
-        self.accept()
+            return self.reject(reason=REASON_NO_CSRF_COOKIE, request=self.request)
+        if not self.validate_csrf_token(token):
+            return self.reject(reason=REASON_BAD_TOKEN, request=self.request)
+        return self.accept()
 
     def get_csrf_token(self) -> Optional[str]:
         return self.request.cookies.get(name_cookie(name=CookieData.CSRFToken.name))
@@ -51,11 +43,7 @@ class CSRFValidationFilter(CSRF, FastAuthConfig):
     def set_csrf_token_cookie(self) -> None:
         self.response.set_cookie(
             key=name_cookie(name=CookieData.CSRFToken.name),
-            value=self.gen_csrf_token(
-                jwt_embedded_value=self.jwt_embedded_value,
-                collision_value=self.collision_value,
-                secrets=self.secrets,
-            ),
+            value=self.gen_csrf_token(),
             max_age=CookieData.CSRFToken.max_age,
             httponly=False,
             secure=self.request.url.is_secure,
@@ -88,7 +76,9 @@ from starlette.middleware.base import BaseHTTPMiddleware
 @final
 class CSRFValidationMiddleware(BaseHTTPMiddleware):
     async def dispatch(
-        self, request: Request, call_next: Callable[[Request], Awaitable[Response]]
+        self,
+        request: Request,
+        call_next: Callable[[Request], Awaitable[Response]],
     ) -> Response:
         response = await call_next(request)
         CSRFValidationFilter(request=request, response=response)()
--- fastauth/csrf.py
+++ fastauth/csrf.py
@@ -1,27 +1,37 @@
 import hmac
 import hashlib
+from os import urandom
 from fastauth._types import FallbackSecrets
+from typing import ClassVar, Optional
+
+
+def _gen_collision_value() -> str:
+    return urandom(16).hex()
 
 
 class CSRF:
+    collision_value: ClassVar[str] = _gen_collision_value()
+    fallback_secrets: ClassVar[Optional[FallbackSecrets]] = None
+    jwt_embedded_value: ClassVar[Optional[str]] = None
+
     @classmethod
-    def validate_csrf_token(cls, token: str, secrets: FallbackSecrets) -> bool:
+    def validate_csrf_token(cls, token: str) -> bool:
+        cls.check_classvars()
         hmac_hash, message_payload = token.split(".")
-        for secret in secrets:
+        for secret in cls.fallback_secrets:  # type: ignore  # we know better
             calculated_hmac = cls.create_hmac(
                 secret=secret, message_payload=message_payload
             )
-            if hmac.compare_digest(calculated_hmac, hmac_hash):
-                return True
+            return hmac.compare_digest(calculated_hmac, hmac_hash)
         return False
 
     @classmethod
-    def gen_csrf_token(
-        cls, jwt_embedded_value: str, collision_value: str, secrets: FallbackSecrets
-    ) -> str:
-        message_payload = jwt_embedded_value + collision_value
+    def gen_csrf_token(cls) -> str:
+        cls.check_classvars()
+        message_payload = cls.jwt_embedded_value + cls.collision_value  # type: ignore
         hmac_hash = cls.create_hmac(
-            secret=secrets.secret_1, message_payload=message_payload
+            secret=cls.fallback_secrets.secret_1,
+            message_payload=message_payload,  # type: ignore
         )
         token = hmac_hash + "." + message_payload
         return token
@@ -33,3 +43,19 @@ class CSRF:
             bytes(message_payload, "utf-8"),
             hashlib.sha256,
         ).hexdigest()
+
+    @classmethod
+    def classvars_are_set(cls) -> bool:
+        if not (
+            cls.fallback_secrets
+            or cls.collision_value
+            or cls.jwt_embedded_value
+            or cls.collision_value
+        ):
+            return False
+        return True
+
+    @classmethod
+    def check_classvars(cls) -> None:
+        if not cls.classvars_are_set():
+            raise ValueError
